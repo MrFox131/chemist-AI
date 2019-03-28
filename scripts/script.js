4871ddbdad1f54a5675fadda1777424b1673be83
cergmin <cergmin@gmail.com>
2019-03-28 11:43:56 +1000

    Scrollbars and Responsive design
    + Added styles for scrollbars
    + Added script to set the recommendation block to the center
    + Added script to change recommendation block size depending on size of the window
    
    P.S.  Recommendation block - golden block with cards, that system recommends

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,6 @@
 var items_in_cart = 0;
 			window.onload = function(event) {
-				$(".jsFromLeft").attr("style", "margin-left: " + $(".content > .card:first").position().left + "px;");
-				$(".jsFromRight").attr("style", "right: " + $(".content > .card:first").position().left + "px;");
-				$(".jsGridWidth").attr("style", $(".jsGridWidth").attr("style") + "width: calc(100vw - (" + ($(".content > .card:first").position().left * 2) + "px));");
-				$(".jsGridWidthPlus").attr("style", $(".jsGridWidthPlus").attr("style") + "width: calc(100vw - (" + (($(".content > .card:first").position().left * 2) - 15) + "px));");
+				onResizeWindow();
 
 				if(items_in_cart > 0){
 					$(".cart_button__text").html("В корзине: " + items_in_cart);
@@ -22,10 +19,33 @@ var items_in_cart = 0;
 			}
 
 			window.onresize = function(event) {
+				onResizeWindow();
+			};
+
+			function onResizeWindow(){
 				$(".jsFromLeft").attr("style", "margin-left: " + $(".content > .card:first").position().left + "px;");
 				$(".jsFromRight").attr("style", "right: " + $(".content > .card:first").position().left + "px;");
 				$(".jsGridWidthPlus").attr("style", $(".jsGridWidthPlus").attr("style") + "width: calc(100vw - (" + (($(".content > .card:first").position().left * 2) - 15) + "px));");
-			};
+				
+				//Set the width and margin-left of the recommendation block
+				if($(window).width() < 1450){
+					$(".jsCenterForRecomend").attr("style", "min-width: 0;");
+				}
+				else{
+					$(".jsCenterForRecomend").attr("style", "min-width: calc((250px + 15px) * 5);");
+				}
+
+				if($(window).width() < 1450){
+					$(".jsCenterForRecomend").attr("style", $(".jsCenterForRecomend").attr("style") + "width: " + ($(window).width() - 120) + "px;");
+					$(".jsCenterForRecomend > .horizontal_scroll").attr("style", "");
+				}
+				else{
+					$(".jsCenterForRecomend").attr("style", $(".jsCenterForRecomend").attr("style") + "display: flex;justify-content: center;");
+					$(".jsCenterForRecomend > .horizontal_scroll").attr("style", "padding-right: 0px;");
+				}
+
+				$(".jsCenterForRecomend").attr("style", $(".jsCenterForRecomend").attr("style") + "margin-left: calc(50vw - " + ($(".jsCenterForRecomend").width() / 2) + "px);");
+			}
 
 			$(".add_to_cart").on("click", function(){
 				if($(this).hasClass("selected")){
