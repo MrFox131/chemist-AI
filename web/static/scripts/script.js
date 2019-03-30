ca29de45fca2615a4edb0301d5998e299da2ffdb
cergmin <cergmin@gmail.com>
2019-03-30 14:29:23 +1000

    Add files via upload
    +Added declension of nouns after numerals

--- web/static/scripts/script.js
+++ web/static/scripts/script.js
@@ -140,3 +140,22 @@ function getAmountInCart(id){
 		}
 	}
 }
+function getNumEnding($number, $endingArray)
+{
+    $number = $number % 100;
+    if ($number>=11 && $number<=19) {
+        $ending=$endingArray[2];
+    }
+    else {
+        $i = $number % 10;
+        switch ($i)
+        {
+            case (1): $ending = $endingArray[0]; break;
+            case (2):
+            case (3):
+            case (4): $ending = $endingArray[1]; break;
+            default: $ending=$endingArray[2];
+        }
+    }
+    return $ending;
+}
