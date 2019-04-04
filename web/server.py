86a23c04688abda714b7cd8d24e193cb879de8b3
solemn-leader <solemnda@gmail.com>
2019-04-04 10:39:38 +1000

    Fixed ajax reqs

--- web/server.py
+++ web/server.py
@@ -13,56 +13,22 @@ app = flask.Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 
-# @app.route('/find_items/<string:request>')
-# def find_items(request):
-#     names, prices, categories, ids = [[] for i in range(4)]
-#     items = db.find_items(request)
-#     for g in items:
-#         names.append(g.name)
-#         prices.append(g.price)
-#         ids.append(g.pk)
-#         categories.append('')
-#     return flask.jsonify(
-#         list(zip(names, prices, categories, ids)),
-#     )
-
-
 @app.route('/get_goods/<int:n_page>')
 def get_goods(n_page):
-    names, prices, categories, image, ids = [[] for i in range(5)]
-    goods = db.get_page_goods(n_page)
-    for g in goods:
-        names.append(g.name)
-        prices.append(g.price)
-        ids.append(g.pk)
-        image.append(g.image)
-        categories.append('')
-    return flask.jsonify(
-        list(zip(names, prices, categories, image, ids)),
-    )
-
-
-@app.route('/get_recs')
-def get_recs():
-    '''return json response with goods we recommend'''
-    names, prices, categories, ids = [[] for i in range(4)]
-    cart_ids, ns = [], []
-    cookie = flask.request.cookies.get('cart', '')
-    if cookie:
-        cookie = urllib.parse.unquote(cookie)
-        cookie = json.loads(cookie)
-    for (pk, n) in cookie:
-        cart_ids.append(pk)
-        ns.append(n)
-    r_names, r_prices, r_categories, r_ids = [[] for i in range(4)]
-    recommendations = rec.get_recs_from_db(cart_ids, ns)
-    for g in recommendations:
-        r_names.append(g.name)
-        r_prices.append(g.price)
-        r_ids.append(g.pk)
-        r_categories.append('')
-    rec_info = zip(r_names, r_prices, r_categories, r_ids)
-    return flask.jsonify(list(rec_info))
+    if flask.request.is_xhr:
+        names, prices, categories, image, ids = [[] for i in range(5)]
+        goods = db.get_page_goods(n_page)
+        for g in goods:
+            names.append(g.name)
+            prices.append(g.price)
+            ids.append(g.pk)
+            image.append(g.image)
+            categories.append('')
+        return flask.jsonify(
+            list(zip(names, prices, categories, image, ids)),
+        )
+    else:
+        return flask.redirect('/')
 
 
 @app.route('/')
